c76b2e48c38583dc7e710cb4dc525d7d53b69cd7
Douglas Estivales <estivaless@gmail.com>
2018-06-24 23:42:16 -0300

    Limites do player

--- Assets/Galaxy Shooter/Scripts/Player.cs
+++ Assets/Galaxy Shooter/Scripts/Player.cs
@@ -3,14 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
+    public float speed = 7.0f;
 
 	// Use this for initialization
 	void Start () {
-        Debug.Log("Hello World");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+        transform.Translate(Vector3.up * Time.deltaTime * speed * verticalInput);
+
+
+        if (transform.position.y < - 4.2f)
+        {
+     
+            transform.position = new Vector3(transform.position.x, -4.2f, 0);
+        }
+
+        if (transform.position.y > 4.2f)
+        {
+
+            transform.position = new Vector3(transform.position.x, 4.2f, 0);
+        }
+
+        if (transform.position.x < -8.2f)
+        {
+
+            transform.position = new Vector3(-8.2f, transform.position.y, 0);
+        }
+
+        if (transform.position.x > 8.2f)
+        {
+
+            transform.position = new Vector3(8.2f, transform.position.y, 0);
+        }
+    }
+
+ 
 }
