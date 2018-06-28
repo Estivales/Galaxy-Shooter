abfa49f9c7d3648a01783a6e9059d75a10c2af9d
Douglas Estivales <estivaless@gmail.com>
2018-06-28 13:14:47 -0300

    Implementado power up

---
+++ Assets/Galaxy Shooter/Scripts/Powerup.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powerup : MonoBehaviour {
+    [SerializeField]
+    private float _speed = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.name == "Player")
+        {
+            Player player = other.GetComponent<Player>();
+            player.canTripleShot = true;
+            Destroy(this.gameObject);
+        }
+        
+   
+    }
+
+    // Update is called once per frame
+    void Update () {
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+
+	}
+}
